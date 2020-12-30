5f69e8d2b0f1784ddbb7173646c1b87c20e3ba63
Arif Cagirici <acagirici23@gmail.com>
2020-12-30 07:27:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @numbers = params[:number]
     @phrase = params[:phrase]
-    phrase_multp = @phrase * number.to_i
+    phrase_multp = @phrase * @number.to_i
     phrase_multp
   end
   
